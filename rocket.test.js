4230ffb51ea85f6fdc4f6d49086d7d74cb9d18c3
Ben Lardie <benlardie@gmail.com>
2019-04-03 19:28:52 -0400

    completed 2 test

--- rocket.test.js
+++ rocket.test.js
@@ -1,21 +1,38 @@
 
-const Rocket = require('./rocket');
+const Rocket = require('./rocket', 'liftOff');
 
 describe('Rocket', () => {
 
   describe('constructor', () => {
     test('it should set default attributes if nothing is passed', () => {
       // ...
+      let newRocket = new Rocket();
+      expect(newRocket).toBeInstanceOf(Rocket);
+      expect(typeof newRocket.name).toBe('string');
+      expect(typeof newRocket.colour).toBe('string');
+
     });
 
     test("it should set the rocket's name if provided", () => {
       // ...
+      let newRocketTwo = new Rocket({name:"Ultimate Rocket"});
+      let result = newRocketTwo.name;
+      expect(result).toBe('Ultimate Rocket');
     });
   });
 
-  describe('liftOff', () => {
+  describe('test liftoff to make sure a flying rocket cant take off', () => {
     // ...
+    let eltonJohn = new Rocket();
+    eltonJohn.flying = true;
+    console.log(eltonJohn)
+    let result = eltonJohn.liftOff();
+    expect(result).toBeFalsy();
+
+
   });
+
+  
 
   // ...
 
